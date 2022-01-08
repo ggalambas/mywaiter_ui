1b9413fc80b95697f7aab662b3e1c1b14f4b7add
agalambas <agalambas@gmail.com>
2022-01-08 23:08:49 +0000

    login page raw

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,28 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       body: Column(
         children: [
-          // spacer
-          // logo
-          // spacer
-          // email input
-          // pass input
-          // forgot pass
-          // login button
-          // or login with
-          Row(
-            children: [
-              // google
-              // facebook
-            ],
+          Expanded(
+            flex: 2,
+            child: Center(
+              child: SvgPicture.asset(
+                'assets/logo.svg',
+                height: 100,
+                color: theme.primaryColor,
+              ),
+            ),
           ),
-          // register
+          Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                    ),
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                    ),
+                  ),
+                  const Text('Forgot your password?'),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Login'),
+                  ),
+                  const Center(
+                    child: Text('or login with'),
+                  ),
+                  Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('G'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('F'),
+                      )
+                    ],
+                  ),
+                  const Center(
+                    child: Text('Don\'t have an account? Register'),
+                  ),
+                  // register
+                ],
+              )),
         ],
       ),
     );
