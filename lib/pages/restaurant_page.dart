35b9a84ecfd53ca707cdf4724905c394c8f64c7e
ggalambas <ggalambas@gmail.com>
2022-01-21 18:46:02 +0000

    spplying sliver bar (incompleted)

--- lib/pages/restaurant_page.dart
+++ lib/pages/restaurant_page.dart
@@ -35,17 +35,37 @@ class _RestaurantPageState extends State<RestaurantPage>
     with SingleTickerProviderStateMixin {
   View view = View.grid;
 
-  late TabController controller = TabController(
+  final expandedHeight = 48;
+  final scrollController = ScrollController();
+  var paddingIncrement = EdgeInsets.zero;
+
+  late final controller = TabController(
     length: categories.length,
     vsync: this,
   );
-  List<String> categories = [
+
+  final categories = [
     'All',
     'Food',
     'Beer',
     'Drinks',
     'Dessert',
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      final expansion =
+          scrollController.offset.clamp(0, expandedHeight) / expandedHeight;
+      setState(() {
+        paddingIncrement = EdgeInsets.only(
+          left: (72 - 16) * expansion,
+          bottom: (16 - 8) * expansion,
+        );
+      });
+    });
+  }
 
   @override
   void dispose() {
@@ -57,13 +77,58 @@ class _RestaurantPageState extends State<RestaurantPage>
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
+      body: CustomScrollView(
+        controller: scrollController,
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: kToolbarHeight + expandedHeight,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'Bolina',
+                style: TextStyle(color: theme.colorScheme.onBackground),
+              ),
+              titlePadding: EdgeInsets.only(left: 16, bottom: 8).add(
+                paddingIncrement,
+              ),
+              background: Container(color: Colors.amber), //!
+            ),
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(LucideIcons.search)),
+              IconButton(
+                  onPressed: () {}, icon: Icon(LucideIcons.clipboardList)),
+              IconButton(
+                onPressed: () => setState(() => view = view.swap()),
+                icon: Icon(view.swapIcon),
+              ),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  color: index.isOdd ? Colors.white : Colors.black12,
+                  height: 100.0,
+                  child: Center(
+                    child: Text('$index', textScaleFactor: 5),
+                  ),
+                );
+              },
+              childCount: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+    return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(onPressed: () {}, icon: Icon(LucideIcons.search)),
           IconButton(onPressed: () {}, icon: Icon(LucideIcons.clipboardList)),
           IconButton(
-              onPressed: () => setState(() => view = view.swap()),
-              icon: Icon(view.swapIcon)),
+            onPressed: () => setState(() => view = view.swap()),
+            icon: Icon(view.swapIcon),
+          ),
         ],
       ),
       body: Column(
