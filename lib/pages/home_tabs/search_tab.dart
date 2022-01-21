0aaac6f815a0f0bd1bd8ddb0008918da2d011047
ggalambas <ggalambas@gmail.com>
2022-01-21 16:31:22 +0000

    item tile

--- lib/pages/home_tabs/search_tab.dart
+++ lib/pages/home_tabs/search_tab.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import 'package:mywaiter_design/config/constants.dart';
 import 'package:mywaiter_design/config/theme/palette.dart';
 import 'package:mywaiter_design/pages/home_page.dart';
 import 'package:mywaiter_design/pages/restaurant_page.dart';
+import 'package:mywaiter_design/widgets/item_tile.dart';
 import 'package:mywaiter_design/widgets/suffix_icon.dart';
 import 'package:mywaiter_design/widgets/svg_icon.dart';
 
@@ -65,52 +65,29 @@ class _SearchTabState extends State<SearchTab> {
 class RestaurantTile extends StatelessWidget {
   final String name = 'Fábrica Bolina';
   final bool opened = true;
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return InkWell(
+    return ItemTile(
+      imageUrl: 'https://via.placeholder.com/'
+          '150/FFD800/FFFFFF/?text=${name.substring(0, 2)}',
+      title: name,
       onTap: () => Navigator.pushNamed(context, RestaurantPage.route),
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: kScreenPadding, vertical: 8),
-        child: Row(
+      subtitle: Text.rich(
+        TextSpan(
+          style: theme.textTheme.caption,
           children: [
-            CircleAvatar(
-              radius: 24,
-              // foregroundImage: NetworkImage(
-              //   'https://via.placeholder.com/'
-              //   '150/FFD800/FFFFFF/?text=${name.substring(0, 2)}',
-              // ),
-              backgroundImage: Svg(
-                'assets/logo.svg',
-                color: theme.colorScheme.onSurface,
-              ),
-              backgroundColor: theme.colorScheme.surface,
-            ),
-            SizedBox(width: 12),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(name, style: theme.textTheme.bodyText1),
-                Text.rich(
-                  TextSpan(
-                    style: theme.textTheme.caption,
-                    children: [
-                      opened
-                          ? TextSpan(
-                              text: 'Opened',
-                              style: TextStyle(color: Palette.green),
-                            )
-                          : TextSpan(
-                              text: 'Closed',
-                              style: TextStyle(color: Palette.red),
-                            ),
-                      TextSpan(text: ' ∙ Closes at 23pm'),
-                    ],
+            opened
+                ? TextSpan(
+                    text: 'Opened',
+                    style: TextStyle(color: Palette.green),
+                  )
+                : TextSpan(
+                    text: 'Closed',
+                    style: TextStyle(color: Palette.red),
                   ),
-                ),
-              ],
-            ),
+            TextSpan(text: ' ∙ Closes at 23pm'),
           ],
         ),
       ),
---
+++ lib/widgets/item_tile.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:mywaiter_design/config/constants.dart';
+
+class ItemTile extends StatelessWidget {
+  final String? imageUrl;
+  final String title;
+  final VoidCallback? onTap;
+  final Widget? subtitle;
+  final Widget? trailing;
+
+  const ItemTile({
+    this.imageUrl,
+    required this.title,
+    this.onTap,
+    this.subtitle,
+    this.trailing,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: kScreenPadding, vertical: 8),
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 24,
+              foregroundImage:
+                  imageUrl != null ? NetworkImage(imageUrl!) : null,
+              backgroundImage: Svg(
+                'assets/logo.svg',
+                color: theme.colorScheme.onSurface,
+              ),
+              backgroundColor: theme.colorScheme.surface,
+            ),
+            SizedBox(width: 12),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: theme.textTheme.bodyText1),
+                if (subtitle != null) subtitle!,
+              ],
+            ),
+            Spacer(),
+            if (trailing != null) trailing!,
+          ],
+        ),
+      ),
+    );
+  }
+}
