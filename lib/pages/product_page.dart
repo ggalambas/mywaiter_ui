dc8429288ca0e0856ef5c42a8ca8f9d63cf64ad8
ggalambas <ggalambas@gmail.com>
2022-01-23 18:10:43 +0000

    product page done

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,8 +1,8 @@
+import 'package:bottom_sheet/bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import 'package:mywaiter_design/config/constants.dart';
-import 'package:mywaiter_design/widgets/parallax.dart';
 import 'package:mywaiter_design/widgets/price.dart';
 
 class ProductPage extends StatefulWidget {
@@ -20,90 +20,130 @@ class _ProductPageState extends State<ProductPage> {
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo sodales dignissim. Etiam dapibus ut massa et pharetra. Praesent sit amet vehicula mauris, id congue nibh. Nullam consectetur pharetra velit, quis auctor magna interdum vitae. Proin enim lectus, eleifend ac neque a, dignissim blandit odio.';
 
   var quantity = 0;
+  void changeQuantity(int increment) => setState(() => quantity += increment);
+
+  double screenHeight(BuildContext context) =>
+      MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+  double screenWidth(BuildContext context) => MediaQuery.of(context).size.width;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    const aspectRatio = 5 / 4;
+    final minHeight =
+        (screenHeight(context) - screenWidth(context) / aspectRatio) /
+            (screenHeight(context) - kToolbarHeight);
     return Scaffold(
-      body: Stack(
-        children: [
-          Parallax(
-            children: [
-              ParallaxItem(
-                offset: -0.2,
-                child: Image(
-                  image: Svg(
-                    'assets/logo.svg',
-                    color: theme.colorScheme.onSurface,
-                  ),
-                ),
-              ),
-            ],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Padding(
+          padding: EdgeInsets.all(8),
+          child: Material(
+            elevation: 4,
+            shape: CircleBorder(),
+            child: BackButton(),
           ),
-          Positioned(
-            top: 264,
-            child: Material(
-              elevation: 2,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(kBorderRadius),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: kScreenPadding,
-                  vertical: 24,
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      category,
-                      style: theme.textTheme.bodyText1!.copyWith(
-                        color: theme.colorScheme.onSurface,
-                      ),
-                    ),
-                    SizedBox(height: 6),
-                    Text(title, style: theme.textTheme.headline4),
-                    SizedBox(height: 24),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Price(price, style: theme.textTheme.headline4),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            quantityButton(
-                              context,
-                              icon: LucideIcons.minus,
-                              onTap: () => setState(() => quantity -= 1),
-                            ),
-                            SizedBox.square(
-                              dimension: 30,
-                              child: Center(
-                                child: Text(
-                                  '$quantity',
-                                  style: theme.textTheme.bodyText2,
-                                ),
-                              ),
-                            ),
-                            quantityButton(
-                              context,
-                              icon: LucideIcons.plus,
-                              onTap: () => setState(() => quantity += 1),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 12),
-                    Text(
-                      description,
-                      style: TextStyle(color: theme.colorScheme.onSurface),
-                    )
-                  ],
+        ),
+      ),
+      extendBodyBehindAppBar: true,
+      backgroundColor: theme.colorScheme.surface,
+      body: Padding(
+        // safe area includes the app bar as well
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: Stack(
+          children: [
+            //? parallax
+            AspectRatio(
+              aspectRatio: aspectRatio,
+              child: Image(
+                image: Svg(
+                  'assets/logo.svg',
+                  color: theme.colorScheme.onSurface,
                 ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: EdgeInsets.only(top: kToolbarHeight),
+              child: FlexibleBottomSheet(
+                minHeight: minHeight,
+                initHeight: minHeight,
+                maxHeight: 1,
+                isCollapsible: false,
+                builder: (context, controller, offset) => Material(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(24),
+                    ),
+                  ),
+                  child: SingleChildScrollView(
+                    controller: controller,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: kScreenPadding,
+                      vertical: 24,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          category,
+                          style: theme.textTheme.bodyText1!.copyWith(
+                            color: theme.colorScheme.onSurface,
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          title,
+                          style: theme.textTheme.headline4!.copyWith(height: 1),
+                        ),
+                        SizedBox(height: 24),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Price(price, style: theme.textTheme.headline4),
+                            StatefulBuilder(builder: (context, setPrice) {
+                              return Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  quantityButton(
+                                    context,
+                                    icon: LucideIcons.minus,
+                                    onTap: () =>
+                                        setPrice(() => changeQuantity(-1)),
+                                  ),
+                                  SizedBox.square(
+                                    dimension: 30,
+                                    child: Center(
+                                      child: Text(
+                                        '$quantity',
+                                        style: theme.textTheme.bodyText2,
+                                      ),
+                                    ),
+                                  ),
+                                  quantityButton(
+                                    context,
+                                    icon: LucideIcons.plus,
+                                    onTap: () =>
+                                        setPrice(() => changeQuantity(1)),
+                                  ),
+                                ],
+                              );
+                            }),
+                          ],
+                        ),
+                        SizedBox(height: 12),
+                        Text(
+                          description,
+                          style: TextStyle(color: theme.colorScheme.onSurface),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
