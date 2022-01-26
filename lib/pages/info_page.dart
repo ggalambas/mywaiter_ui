33e771e4725ec029eda74468191b0916e56adfc9
ggalambas <ggalambas@gmail.com>
2022-01-26 11:44:30 +0000

    closed option on restaurant schedule

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -5,13 +5,17 @@ import 'package:mywaiter_design/widgets/sheet_scaffold.dart';
 
 class Schedule {
   final int _weekday;
-  final TimeOfDay start;
-  final TimeOfDay end;
-  const Schedule(this._weekday, this.start, this.end)
-      : assert(1 <= _weekday && _weekday <= 7);
+  final TimeOfDay? start;
+  final TimeOfDay? end;
+  const Schedule(this._weekday, [this.start, this.end])
+      : assert(1 <= _weekday && _weekday <= 7),
+        assert((start == null) ^ (end != null));
 
   String get weekday => _names[_weekday]!;
   bool get isToday => _weekday == DateTime.now().weekday;
+  String date(BuildContext context) => start == null
+      ? 'Closed'
+      : '${start!.format(context)} - ${end!.format(context)}';
 
   final _names = const {
     DateTime.monday: 'Monday',
@@ -28,8 +32,7 @@ class InfoPage extends StatelessWidget {
   static String route = '/info';
 
   final schedule = [
-    Schedule(DateTime.monday, TimeOfDay(hour: 16, minute: 0),
-        TimeOfDay(hour: 23, minute: 0)),
+    Schedule(DateTime.monday),
     Schedule(DateTime.tuesday, TimeOfDay(hour: 16, minute: 0),
         TimeOfDay(hour: 23, minute: 0)),
     Schedule(DateTime.wednesday, TimeOfDay(hour: 16, minute: 0),
@@ -75,11 +78,7 @@ class InfoPage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(day.weekday, style: style),
-                      Text(
-                        '${day.start.format(context)} - '
-                        '${day.end.format(context)}',
-                        style: style,
-                      ),
+                      Text(day.date(context), style: style),
                     ],
                   );
                 }),
